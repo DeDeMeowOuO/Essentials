87667836148f07d753095832b43b8ff345e53b5f
gamerfreak22222 <>
2013-07-18 16:01:17 -0400

    Updating Spigot Reference

--- src/com/earth2me/essentials/Essentials.java
+++ src/com/earth2me/essentials/Essentials.java
@@ -81,7 +81,7 @@ import org.yaml.snakeyaml.error.YAMLException;
 public class Essentials extends MPlugin implements net.ess3.api.IEssentials
 {
 	public static final int BUKKIT_VERSION = 2812;
-        public static final int SPIGOT_VERSION = 1018;
+        public static final int SPIGOT_VERSION = 1033;
 	private static final Logger LOGGER = Logger.getLogger("Minecraft");
 	private transient ISettings settings;
 	private final transient TNTExplodeListener tntListener = new TNTExplodeListener(this);
